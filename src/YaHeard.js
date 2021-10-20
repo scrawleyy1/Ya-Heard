fb3867454aa01b649919130f49100c1dc41c8c12
scrawleyy1 <crawley.sydney@gmail.com>
2021-10-20 10:10:44 -0500

    first push

---
+++ src/YaHeard.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './YaHeard.css';
+
+function YaHeard() {
+  return (
+    <div className="YaHeard">
+      <h1>Ya Heard?</h1>
+    </div>
+  );
+}
+
+export default YaHeard;
